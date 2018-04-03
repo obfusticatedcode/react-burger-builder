5c4dcc76c115d65ea1b2835162f566e8603fcf0c
Chisomo Lungu <chisomo@gmail.com>
2018-04-03 23:43:49 +0100

    implementing a side drawer for responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css'
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>
       {props.children}
     </main>
