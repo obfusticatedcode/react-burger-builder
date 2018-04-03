fd5852644f3e58eb2710eeeec673220c5046b04e
Chisomo Lungu <chisomo@gmail.com>
2018-04-04 00:54:50 +0100

    implemented hamburger menu icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => {
   return (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 }
- 
+
 export default drawerToggle;
